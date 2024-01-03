9860fb56af3d3c89b93ac1fb6beb3afffbaf018f
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-04 02:32:07 +0800

    progress

--- src/layout/sections/Header.jsx
+++ src/layout/sections/Header.jsx
@@ -8,8 +8,7 @@ function Header({toggleTheme}) {
     <>
       <header id="header">
         <nav id="main-nav">
-          <Stack className="nav-links" direction="row" spacing={3} alignItems="center">
-            <span></span>
+          <Stack className="nav-links" direction="row" spacing={3} alignItems="center">            
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
             <Button className="toggle-theme-btn" variant="contained" onClick={toggleTheme}>Toggle Theme</Button>
