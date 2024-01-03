a41fac51c92c2df72a1ad369a519a747adc61b4b
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-04 02:50:37 +0800

    fixed error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,44 @@
+import { useState, useMemo } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/system/Box";
 import Header from "../layout/sections/Header";
 
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 function ErrorPage() {
+  const [currentTheme, setCurrentTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    console.log("toggling theme");
+    setCurrentTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  const theme = useMemo(() => {
+    return currentTheme === "light" ? lightTheme : darkTheme;
+  }, [currentTheme]);
+
   return (
     <>
-      <Header />
-      <main>
-        <h1>404 Page Not Found.</h1>
-      </main>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box id="page-wrap" className={`theme-${currentTheme}`}>
+          <Header toggleTheme={toggleTheme} />
+          <main>
+            <h1>Something went wrong.</h1>
+          </main>
+        </Box>
+      </ThemeProvider>
     </>
   );
 }
