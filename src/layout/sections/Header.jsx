a0ec81114c4260015045842cbf8e163b25414026
jerome-shiftleft <jerome@theshiftleft.com>
2024-01-04 00:28:15 +0800

    toggle theme button align right

--- src/layout/sections/Header.jsx
+++ src/layout/sections/Header.jsx
@@ -1,14 +1,16 @@
 import { NavLink } from "react-router-dom";
 import Stack from '@mui/material/Stack';
+import { Button } from "@mui/material";
 
 function Header() {
   return (
     <>
-      <header>
+      <header id="header">
         <nav id="main-nav">
-          <Stack className="nav-links" direction="row" spacing={3}>
+          <Stack className="nav-links" direction="row" spacing={3} alignItems="center">
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>            
+            <NavLink to="/about">About</NavLink>
+            <Button className="toggle-theme-btn" variant="contained">Toggle Theme</Button>
           </Stack>
         </nav>
       </header>
